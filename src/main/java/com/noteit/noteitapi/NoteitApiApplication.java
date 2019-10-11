cf313b355f9274ef30542fc20e4839c1b75eb26d
Jose Eduardo Baia <joseeduardobaia@MacBook-Pro-de-Jose.local>
2019-10-12 00:43:56 +0100

    Initial commit
    Note it
    
    Application of notes.
     Allows user to create notes in notebooks.
    Allow Create/ update/ delete notes and notebooks
    Allow users to filter notes
    Allow users send feedbacks by email
    
    Backend
    Business logic
    Persistence
    Security (Authentication+ Authorization)
    User input validation
    Communication Contracts ( REST endpoints)

---
+++ src/main/java/com/noteit/noteitapi/NoteitApiApplication.java
@@ -0,0 +1,13 @@
+package com.noteit.noteitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteitApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/noteit/noteitapi/model/Note.java
@@ -0,0 +1,66 @@
+package com.noteit.noteitapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class Note {
+    @Id
+    private UUID id;
+    private String title;
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Notebook notebook;
+
+    private Date lastModifiedOn;
+
+    protected Note() {
+        this.id = UUID.randomUUID();
+        this.lastModifiedOn = new Date();
+    }
+
+
+    public Note(String title, String text, Notebook notebook) {
+        this();
+        this.title = title;
+        this.text = text;
+        this.notebook = notebook;
+    }
+
+    public Note(String id, String title, String text, Notebook notebook) {
+        this(title, text, notebook);
+        if (id != null) {
+            this.id = UUID.fromString(id);
+        }
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
